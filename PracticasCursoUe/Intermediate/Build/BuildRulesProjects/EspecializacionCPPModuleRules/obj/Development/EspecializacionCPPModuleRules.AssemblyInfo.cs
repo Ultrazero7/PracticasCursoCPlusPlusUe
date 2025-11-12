a12ad3f663c957a89acd5c25b56488fe6ea4dcbd
ZeroPluz <marcodurantorres7@gmail.com>
2025-11-11 21:41:44 -0600

    actualizacion

--- PracticasCursoUe/Intermediate/Build/BuildRulesProjects/EspecializacionCPPModuleRules/obj/Development/EspecializacionCPPModuleRules.AssemblyInfo.cs
+++ PracticasCursoUe/Intermediate/Build/BuildRulesProjects/EspecializacionCPPModuleRules/obj/Development/EspecializacionCPPModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EspecializacionCPPModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2721b2cf88cff16411b2e86afe2d8be036d784")]
 [assembly: System.Reflection.AssemblyProductAttribute("EspecializacionCPPModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EspecializacionCPPModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
